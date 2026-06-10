7a93fdaf602af165140af74a6e6ae71826d51f9b
avelev01 <165008664+avelev01@users.noreply.github.com>
2026-06-10 18:40:59 +0300

    Update config.py

--- sapdi/config.py
+++ sapdi/config.py
@@ -9,4 +9,8 @@ class Config:
     PASSWORD = os.getenv("SAPDI_PASSWORD")
     TENANT = os.getenv("SAPDI_TENANT", "default")
     USERNAME = os.getenv("SAPDI_USERNAME")
-   
+    
+
+
+if not Config.BASE_URL or not Config.USERNAME or not Config.PASSWORD:
+    raise Exception("Missing SAP DI environment variables")
