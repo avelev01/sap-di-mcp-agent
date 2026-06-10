f69dad02e51c2ed33a65e182931b324be2b3adcc
avelev01 <165008664+avelev01@users.noreply.github.com>
2026-06-10 17:55:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,13 @@
 from sapdi.context import MCPContext
+from sapdi.registry import bind_tools
+from mcp import MCPServer
 
-# created once when MCP server starts
+server = MCPServer()
+
+# create shared SAP DI session ONCE
 context = MCPContext()
+
+# bind all tools using factory
+bind_tools(server, context)
+
+server.run()
