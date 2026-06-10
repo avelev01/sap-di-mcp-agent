c79bce43714970fe0d0adefe4583feaad0f07e59
avelev01 <165008664+avelev01@users.noreply.github.com>
2026-06-10 17:44:55 +0300

    Update config.py

--- sapdi/config.py
+++ sapdi/config.py
@@ -5,13 +5,7 @@ load_dotenv()
 
 
 class Config:
-    SAP_DI_BASE_URL = os.getenv(
-        "SAP_DI_URL",
-        "https://vsystem.ingress.dh-1ts46knm.dhaas-live.shoot.live.k8s-hana.ondemand.com"
-    ).rstrip("/")
-    SAP_DI_TENANT = os.getenv("SAP_DI_TENANT", "......")
-    SAP_DI_USERNAME = os.getenv("SAP_DI_USERNAME", ".....")
-    SAP_DI_PASSWORD = os.getenv("SAP_DI_PASSWORD", "......")
-    SAP_DI_API_PREFIX = os.getenv("SAP_DI_API_PREFIX", "/app/pipeline-modeler/service/v1")
-    SAP_DI_VERIFY_SSL = os.getenv("SAP_DI_VERIFY_SSL", "true").lower() in ("1", "true", "yes", "on")
-    SESSION_CACHE = os.getenv("SAP_DI_SESSION_CACHE", "false").lower() in ("1", "true", "yes", "on")
+    BASE_URL = os.getenv("SAPDI_BASE_URL")
+    TENANT = os.getenv("SAPDI_TENANT", "default")
+    USERNAME = os.getenv("SAPDI_USERNAME")
+    PASSWORD = os.getenv("SAPDI_PASSWORD")
